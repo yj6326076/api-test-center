4efcace8c5d0f0a1c6fcc2b5b18b8a39c9ca3508
yj6326076 <yj6326076@hotmail.com>
2021-11-29 21:46:17 +0800

    init project

---
+++ src/main/java/com/little/smaile/ApiTestCenterApplication.java
@@ -0,0 +1,13 @@
+package com.little.smaile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/little/smaile/ApiTestCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.little.smaile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
